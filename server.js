625d668b35056dc592f100f5d4d7374c9d7796c4
Letícia Rodrigues <leticiab.rodrigues@outlook.com>
2022-07-29 22:56:58 -0300

    uploading final changes :recycle:

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const app = require('./src/app')
 
 const PORT = process.env.PORT || 9000
 
-app.listen(PORT, () => console.log(`server rodando uhulll! só testandooooo na porta ${PORT}`))
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
