64a5e5396b8306abb239122c0078373692bd3567
Letícia Rodrigues <leticiab.rodrigues@outlook.com>
2022-07-30 00:38:09 -0300

    refactoring code :recycle:

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 router.get('/', function (req, res) {
     res.status(200).send({
         title: "Reprograma projeto final - inSTEAM",
-        version: "1:0:0"
+        version: "1.0.0"
     })
 })
 
